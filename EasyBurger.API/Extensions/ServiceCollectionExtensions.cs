ffcb95c6d6c34cd3aac344f797ff569f6b11745a
Matheus Carvalho <matheus.galdino@ed.dev.br>
2022-09-18 18:15:19 -0300

    Criando metodos de extensão para DI

---
+++ EasyBurger.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,62 @@
+﻿using EasyBurger.API.Infra.Contexts;
+using EasyBurger.API.Infra.Repositories;
+using EasyBurger.API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace EasyBurger.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMyDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApiContext>(options =>
+            {
+                var conString = configuration.GetConnectionString("MySql");
+                options.UseMySql(conString, ServerVersion.AutoDetect(conString));
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddMyRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<UserRepository>();
+            services.AddTransient<ProductRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddMyServices(this IServiceCollection services)
+        {
+            services.AddSingleton<TokenService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddMyAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            var key = Encoding.ASCII.GetBytes(configuration["SECRET_KEY"]);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
+    }
+}
--- EasyBurger.API/Program.cs
+++ EasyBurger.API/Program.cs
@@ -1,11 +1,5 @@
 
-using EasyBurger.API.Infra.Contexts;
-using EasyBurger.API.Infra.Repositories;
-using EasyBurger.API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using EasyBurger.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,34 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApiContext>(options =>
-{
-    var conString = builder.Configuration.GetConnectionString("MySql");
-    options.UseMySql(conString, ServerVersion.AutoDetect(conString));
-});
+builder.Services.AddMyDbContext(builder.Configuration);
+builder.Services.AddMyRepositories();
+builder.Services.AddMyServices();
 
-builder.Services.AddTransient<UserRepository>();
-builder.Services.AddTransient<ProductRepository>();
-
-builder.Services.AddSingleton<TokenService>();
-
-var key = Encoding.ASCII.GetBytes(builder.Configuration["SECRET_KEY"]);
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+builder.Services.AddMyAuth(builder.Configuration);
 
 var app = builder.Build();
 
